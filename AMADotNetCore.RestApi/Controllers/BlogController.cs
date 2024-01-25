6baed6f809e79b115e5672d7a0ae1dbc04bc17c0
yasuoOwO <heinmina05@gmail.com>
2024-01-26 00:51:00 +0630

    [Complete] Rest Api Pagination , Create

--- AMADotNetCore.RestApi/Controllers/BlogController.cs
+++ AMADotNetCore.RestApi/Controllers/BlogController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using AMADotNetCore.RestApi.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace AMADotNetCore.RestApi.Controllers
@@ -7,18 +8,53 @@ namespace AMADotNetCore.RestApi.Controllers
     [ApiController]
     public class BlogController : ControllerBase
     {
+        private readonly AppDbContext db = new AppDbContext();
         [HttpGet]
         public IActionResult GetBlogs()
         {
-            AppDbContext db = new AppDbContext();
             var lst = db.Blogs.ToList();
             return Ok(lst);
         }
 
-        [HttpPost]
-        public IActionResult CreateBlogs()
+        [HttpGet("{pageNo}/{pageSize}")]
+        public IActionResult GetBlogs(int pageNo, int pageSize)
         {
-            return Ok("post");
+            //pageNo = 1 [1-10]
+            //pageNo = 2 [11-20]
+            //endRowNo = pageNo * pageSize ; 10
+            //startRowNo = endRowNo - pageSize ; 6570  - 10 = 6560 + 1 = 6561
+            
+            var lst = db.Blogs
+                 .Skip((pageNo - 1) * pageSize)
+                 .Take(pageSize)
+                .ToList();
+            var rowCount = db.Blogs.Count();
+            var pageCount = rowCount / pageSize; // 110 / 10  = 11
+            if(rowCount % pageSize > 0) 
+            {
+                pageCount++;
+            }
+            return Ok( new { isEndOfPage = pageNo >= pageCount, PageCount = pageCount,PageNo = pageNo , PageSize = pageSize,Data = lst });
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetBlog(int id)
+        {
+            var item = db.Blogs.FirstOrDefault(x => x.Blog_Id == id);
+            if (item is null)
+            {
+                return NotFound("data not found .");
+            }
+            return Ok(item);
+        }
+
+        [HttpPost]
+        public IActionResult CreateBlogs(BlogDataModel blog)
+        {
+            db.Blogs.Add(blog);
+            var result = db.SaveChanges();
+            var message = result > 0 ? "Saving Success." : "Saving Failed.";
+            return Ok(message);
         }
 
         [HttpPut]
